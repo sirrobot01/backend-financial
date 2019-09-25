ad4bd2d260aa1de0dd7c66c4698bc66e311c39e6
sirrobot01 <akeremukhtar10@gmail.com>
2019-09-25 10:47:36 +0100

    Fix gitignore

---
+++ backend/urls.py
@@ -0,0 +1,41 @@
+"""backend URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from .views import ObtainJWT
+from rest_framework_jwt.views import verify_jwt_token
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Team Trion Financial API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', ObtainJWT.as_view(), name='token'),
+    path('register/', include('rest_auth.registration.urls')),
+    path('token-refresh/', verify_jwt_token, name='token_refresh'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
+    path('', include('api.urls'))
+    ]
